479e02ef326ad5a2eda1d07236d42423c8a7ee7b
Ivan <efcolipt@gmail.com>
2024-07-02 19:52:10 +0300

    chore: test

--- public/web-app.js
+++ public/web-app.js
@@ -1,68 +1,141 @@
 const app = window.Telegram.WebApp;
-const storage = app.CloudStorage;
+// const storage = app.CloudStorage;
 
-const ws = new WebSocket("wss://echo.websocket.org/");
+const conn = new WebSocket("ws://vn1840.vps-server.ru:5017");
 
-ws.onopen = function (event) {
-	ws.send(JSON.stringify({
-		type: "message",
-		text: 'test',
-		id: 'test',
-		date: Date.now(),
-	}));
-};
+class CryptoManager {
+    generateKeyPair() {
+        return Crypto.generateKeyPairSync("rsa", { modulusLength: 2048, })
+    }
 
-ws.onmessage = function (event) {
-	console.log(event.data);
-};
+    encryptMessage(packageData, hashKey, serverCertificate) {
+        const stringForHash = "type=echo&data.message=" + packageData.data.message;
 
+        const signature = Crypto.createHmac("sha256", hashKey).update(stringForHash).digest('hex').toUpperCase();
+
+        packageData.signature = signature;
+
+        const aesKey = Crypto.randomBytes(32);
+        const aesIV = Crypto.randomBytes(16);
+
+
+        let cipher = Crypto.createCipheriv('aes-256-cbc', aesKey, aesIV);
+
+        let encrypted = cipher.update(JSON.stringify(packageData), 'utf8', 'base64');
+
+        encrypted += cipher.final('base64');
+
+        const result = Buffer.from(encrypted, 'base64');
+
+        const cipherSecret = Crypto.publicEncrypt(
+            {
+                key: serverCertificate,
+                padding: Crypto.constants.RSA_PKCS1_PADDING,
+                oaepHash: "sha256",
+            },
+            Buffer.concat([aesKey, aesIV])
+        );
+
+        return Buffer.concat([cipherSecret, result]).toString('base64');
+    }
+
+    decryptMessage(ciphered, privateKey) {
+        const bytes = Buffer.from(ciphered, 'base64')
+        const cipheredSecret = bytes.slice(0, 256);
+        const payload = bytes.slice(256, bytes.length);
+
+        const secret = Crypto.privateDecrypt(
+            {
+                key: privateKey,
+                padding: Crypto.constants.RSA_PKCS1_PADDING,
+                oaepHash: "sha256",
+            },
+            cipheredSecret
+        );
+
+        const aesKey = secret.slice(0, 32);
+        const aesIV = secret.slice(32, 48);
+
+        const decipher = Crypto.createDecipheriv('aes-256-cbc', aesKey, aesIV);
+        const decrypted = decipher.update(payload, 'base64', 'utf8');
+
+        return JSON.parse((decrypted + decipher.final('utf8')));
+    }
+}
+
+class Fetcher {
+    #CryptoManager = new CryptoManager()
+
+    #isWasInitRequest = false
+
+    #connection = null
+
+    #cryptoKeys = {
+        publicKey: null, 
+        privateKey: null
+    }
+
+    constructor(connection) {
+        this.#connection = connection
+
+        this.#init()
+    }
+
+    #init() {
+		this.#connection.onopen = () => {
+			this.#cryptoKeys = this.#CryptoManager.generateKeyPair();
+			this.send({
+				publicKey: this.#cryptoKeys.publicKey.export({ type: "pkcs1", format: "pem" })
+			});
+		}
+		
+
+        this.#connection.onclose = (error) => {
+            console.log("Connection Error: " + error.toString());
+        };
+    
+        this.#connection.onerror = () => {
+            console.log('echo-protocol Connection Closed');
+        }
+
+        this.#connection.onmessage = (e) => this.#receive(e.data)
+    }
+
+    #receive(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: " + message.utf8Data);
+        }
+
+        if (!this.#isWasInitRequest) {
+            const response = this.#CryptoManager.decryptMessage(message.utf8Data, this.#cryptoKeys.privateKey);
+
+            const request = this.#CryptoManager.encryptMessage(this.#toRequest({ message: '' }, 'echo'), response.data.hashKey, response.data.publicKey)
+
+            this.#connection.send(request);
+
+            this.#isWasInitRequest = true;
+        }
+    }
+
+    send(data, type = 'init', signature = '') {
+        const request = this.#toRequest(data, type, signature);
+
+        this.#connection.send(JSON.stringify(request));
+    }
+
+    #toRequest(data, type = 'init', signature = '') {
+        return {
+            type,
+            data,
+            signature
+        }
+    }
+}
+
+
+const fetcher = new Fetcher(conn)
 
 app.ready();
 app.expand();
-
-const [errorer, btn, reset, count] = [
-	/** @type {HTMLParagraphElement} */ (document.getElementById("errors")),
-	/** @type {HTMLButtonElement} */ (document.getElementById("click")),
-	/** @type {HTMLButtonElement} */ (document.getElementById("reset")),
-	/** @type {HTMLParagraphElement} */ (document.getElementById("count")),
-];
-
-/** @param e {any} */
-const setError = e =>
-	(errorer.innerText += "\n" + JSON.stringify(e, null, "\t"));
-
-window.addEventListener("error", e => setError(e.toString()));
-
-/** @param error {string | null} @param value {string | undefined} */
-const parse = (error, value) => (!error ? (value ? parseInt(value) : 0) : 0);
-
-storage.getItem("clicks", (error, value) => {
-	if (error) return setError({ error, type: typeof error, value });
-	btn.removeAttribute("disabled");
-
-	const c = parse(error, value);
-	count.innerText = `Clicked ${c} times`;
-});
-
-btn.addEventListener("click", () => {
-	btn.setAttribute("disabled", "");
-	storage.getItem("clicks", (error, value) => {
-		if (error) return setError({ error, type: typeof error, value });
-		const c = parse(error, value) + 1;
-		storage.setItem("clicks", String(c), (error, success) => {
-			if (error) return setError({ error, type: typeof error, success });
-			count.innerText = `Clicked ${c} times`;
-			btn.removeAttribute("disabled");
-		});
-	});
-});
-
-reset.addEventListener("click", () => {
-	storage.removeItem("clicks", (error, success) => {
-		if (error) return setError({ error, type: typeof error, success });
-		count.innerText = `Count was reset!`;
-		btn.removeAttribute("disabled");
-	});
-});
 
 export {};
